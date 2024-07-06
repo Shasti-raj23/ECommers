edd0e72e8227b668dfc7e7ca74d7d7fc1370d0a3
Shasti-raj23 <shastiraj35@gmail.com>
2024-07-06 23:07:50 +0530

    Added some content like card at the end of day 1

---
+++ index.js
@@ -0,0 +1,15 @@
+import Express from 'express'
+import {dirname} from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const App =Express()
+
+
+
+App.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/main.html")
+})
+App.listen(3300,()=>{
+    console.log("Server is running on port 3300")
+})
