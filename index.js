3a516ca72974ceee35a6eb912834d9085dbbfe5e
Shasti-raj23 <shastiraj35@gmail.com>
2024-07-07 22:57:31 +0530

    day 2

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 import Express from 'express'
 import {dirname} from 'path'
 import { fileURLToPath } from 'url'
+import bodyParser from 'body-parser'
 import fs from 'fs'
 const rawData = fs.readFileSync('data.json')
 const data = JSON.parse(rawData);
-const data1=data.BatteryHolder
+let data1=data.batteryholder
 console.log(data1)
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const App =Express()
 
 App.use(Express.static(__dirname))
+App.use(bodyParser.urlencoded({extended:false}))
 App.get("/search",(req,res)=>{
     res.render(__dirname+"/Roboat.ejs",{data:data1})
 })
 App.get("/",(req,res)=>{
     res.sendFile(__dirname+"/main.html")
+})
+App.post("/category",(req,res)=>{
+    const query = req.body.category.toLowerCase();
+    // console.log(typeof(data))
+    const result = data.find(item => item.includes(query));
+    res.render(__dirname+"Roboat.ejs",{data:result})
 })
 App.listen(3300,()=>{
     console.log("Server is running on port 3300")
