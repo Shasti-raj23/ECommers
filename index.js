865bdc1c092ee83be7a3387c7799648c9164e29d
Shasti-raj23 <shastiraj35@gmail.com>
2024-07-07 22:17:43 +0530

    this is end of day to where the json file is add for template for many category

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import Express from 'express'
 import {dirname} from 'path'
 import { fileURLToPath } from 'url'
+import fs from 'fs'
+const rawData = fs.readFileSync('data.json')
+const data = JSON.parse(rawData);
+const data1=data.BatteryHolder
+console.log(data1)
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const App =Express()
 
 App.use(Express.static(__dirname))
-
+App.get("/search",(req,res)=>{
+    res.render(__dirname+"/Roboat.ejs",{data:data1})
+})
 App.get("/",(req,res)=>{
     res.sendFile(__dirname+"/main.html")
 })
